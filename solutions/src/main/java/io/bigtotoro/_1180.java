e61de8741421cc4664b0bfc36fa6cb41196f5fe7
sskuratov <s.a.skuratov@gmail.com>
2020-10-12 16:36:03 +0600

    Added 1180

--- solutions/src/main/java/io/bigtotoro/_1180.java
+++ solutions/src/main/java/io/bigtotoro/_1180.java
@@ -23,4 +23,33 @@ public class _1180 {
             return result;
         }
     }
+
+    public static class Solution2 {
+        public int countLetters(String S) {
+            int count = 1;
+            int result = 0;
+
+            for (int i = 1; i < S.length(); ++i) {
+                if (S.charAt(i) == S.charAt(i - 1)) {
+                    ++count;
+                } else {
+                    result += accumulate(count);
+                    count = 1;
+                }
+            }
+
+            result += accumulate(count);
+
+            return result;
+        }
+
+        private int accumulate(int n) {
+            if (n == 1) {
+                return 1;
+            }
+
+            return n + accumulate(n - 1);
+        }
+    }
+
 }
--- solutions/src/test/java/io/bigtotoro/_1180_Test.java
+++ solutions/src/test/java/io/bigtotoro/_1180_Test.java
@@ -8,19 +8,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class _1180_Test {
 
     private static _1180.Solution1 solution1;
+    private static _1180.Solution2 solution2;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _1180.Solution1();
+        solution2 = new _1180.Solution2();
     }
 
     @Test
     public void test11() {
         assertThat(solution1.countLetters("aaaba")).isEqualTo(8);
+        assertThat(solution2.countLetters("aaaba")).isEqualTo(8);
     }
 
     @Test
     public void test12() {
         assertThat(solution1.countLetters("aaaaaaaaaa")).isEqualTo(55);
+        assertThat(solution2.countLetters("aaaaaaaaaa")).isEqualTo(55);
     }
 }

cb6e58287c7caa6f00ec1d0a0a0d3a8ce2c3cc54
sskuratov <s.a.skuratov@gmail.com>
2020-10-14 14:44:26 +0600

    Added 811

--- solutions/src/main/java/io/bigtotoro/_811.java
+++ solutions/src/main/java/io/bigtotoro/_811.java
@@ -1,9 +1,10 @@
 package io.bigtotoro;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * <a href="https://leetcode.com/problems/subdomain-visit-count/">811. Subdomain Visit Count</a>
@@ -14,11 +15,12 @@ public class _811 {
         public List<String> subdomainVisits(String[] cpdomains) {
             Map<String, Integer> domainsMap = new HashMap<>();
 
-            for (String domain : cpdomains) {
+            Arrays.stream(cpdomains).forEach(domain -> {
                 int spaceDiv = domain.indexOf(" ");
                 Integer cp = Integer.valueOf(domain.substring(0, spaceDiv));
                 String[] subdomains = domain.substring(spaceDiv + 1).split("\\.");
                 String subdomain = "";
+
                 for (int i = subdomains.length - 1; i >= 0; --i) {
                     if (i == subdomains.length - 1) {
                         subdomain = subdomains[i];
@@ -28,12 +30,13 @@ public class _811 {
 
                     domainsMap.compute(subdomain, (k, v) -> (v == null) ? cp : v + cp);
                 }
-            }
+            });
 
-            List<String> result = new ArrayList<>();
-            for (Map.Entry<String, Integer> e : domainsMap.entrySet()) {
-                result.add("" + e.getValue() + " " + e.getKey());
-            }
+            List<String> result = domainsMap
+                    .entrySet()
+                    .stream()
+                    .map(e -> "" + e.getValue() + " " + e.getKey())
+                    .collect(Collectors.toList());
 
             return result;
         }

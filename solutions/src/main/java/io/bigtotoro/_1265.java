9bfd81c3d3c2f99476207cd70e72b1a68cd4dd59
sskuratov <s.a.skuratov@gmail.com>
2020-10-19 08:26:08 +0600

    Added 1265

---
+++ solutions/src/main/java/io/bigtotoro/_1265.java
@@ -0,0 +1,23 @@
+package io.bigtotoro;
+
+/**
+ * <a href="https://leetcode.com/problems/print-immutable-linked-list-in-reverse/">1265. Print Immutable Linked List in Reverse</a>
+ */
+
+ interface ImmutableListNode {
+     void printValue(); // print the value of this node.
+     ImmutableListNode getNext(); // return the next node.
+ };
+
+public class _1265 {
+    public static class Solution {
+        public void printLinkedListInReverse(ImmutableListNode head) {
+            if (head == null) {
+                return;
+            }
+
+            printLinkedListInReverse(head.getNext());
+            head.printValue();
+        }
+    }
+}

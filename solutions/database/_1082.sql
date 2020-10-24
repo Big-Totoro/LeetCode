adf6924df1c9a7cb6d767d1ade7ad0dbe050c88a
sskuratov <s.a.skuratov@gmail.com>
2020-10-24 17:04:43 +0600

    Added 1082

---
+++ solutions/database/_1082.sql
@@ -0,0 +1,14 @@
+/**
+ * <a href="https://leetcode.com/problems/sales-analysis-i/">Sales Analysis I</a>
+ */
+
+/**
+  Solution 1
+ */
+SELECT seller_id
+FROM (
+    SELECT *, RANK() OVER (ORDER BY SUM(price) DESC) total
+    FROM Sales
+    GROUP BY seller_id
+    ) AS t
+WHERE t.total = 1;

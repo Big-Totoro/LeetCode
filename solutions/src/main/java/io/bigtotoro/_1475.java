aee99f8987d81bf54158b69e1d8504d7a8d011aa
sskuratov <s.a.skuratov@gmail.com>
2020-11-16 11:17:39 +0600

    Added 1475 Solution #2

--- solutions/src/main/java/io/bigtotoro/_1475.java
+++ solutions/src/main/java/io/bigtotoro/_1475.java
@@ -1,12 +1,13 @@
 package io.bigtotoro;
 
+import java.util.Stack;
+
 /**
  * <a href="https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/">1475. Final Prices With a Special Discount in a Shop</a>
  */
 
 public class _1475 {
     public static class Solution1 {
-
         public int[] finalPrices(int[] prices) {
             for (int i = 0; i < prices.length - 1; i++) {
                 int price = findMin(prices, i + 1, prices[i]);
@@ -24,6 +25,21 @@ public class _1475 {
             }
 
             return 0;
+        }
+    }
+
+    public static class Solution2 {
+        public int[] finalPrices(int[] prices) {
+            Stack<Integer> stack = new Stack<>();
+
+            for (int i = 0; i < prices.length; ++i) {
+                while (!stack.empty() && prices[i] <= prices[stack.peek()]) {
+                    prices[stack.pop()] -= prices[i];
+                }
+                stack.push(i);
+            }
+
+            return prices;
         }
     }
 }
--- solutions/src/test/java/io/bigtotoro/_1475_Test.java
+++ solutions/src/test/java/io/bigtotoro/_1475_Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class _1475_Test {
 
     private static _1475.Solution1 solution1;
+    private static _1475.Solution2 solution2;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _1475.Solution1();
+        solution2 = new _1475.Solution2();
     }
 
     @Test
@@ -32,6 +34,27 @@ public class _1475_Test {
     @Test
     public void test14() {
         assertThat(solution1.finalPrices(new int[] {8, 7, 4, 2, 8, 1, 7, 7, 10, 1}))
+                .isEqualTo(new int[] {1, 3, 2, 1, 7, 0, 0, 6, 9, 1});
+    }
+
+    @Test
+    public void test21() {
+        assertThat(solution2.finalPrices(new int[] {8, 4, 6, 2, 3})).isEqualTo(new int[] {4, 2, 4, 2, 3});
+    }
+
+    @Test
+    public void test22() {
+        assertThat(solution2.finalPrices(new int[] {1, 2, 3, 4, 5})).isEqualTo(new int[] {1, 2, 3, 4, 5});
+    }
+
+    @Test
+    public void test23() {
+        assertThat(solution2.finalPrices(new int[] {10, 1, 1, 6})).isEqualTo(new int[] {9, 0, 1, 6});
+    }
+
+    @Test
+    public void test24() {
+        assertThat(solution2.finalPrices(new int[] {8, 7, 4, 2, 8, 1, 7, 7, 10, 1}))
                 .isEqualTo(new int[] {1, 3, 2, 1, 7, 0, 0, 6, 9, 1});
     }
 }

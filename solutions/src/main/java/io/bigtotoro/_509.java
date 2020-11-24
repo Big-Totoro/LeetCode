e597dca0b7518a5ab4f511b6b0385cb735501abe
sskuratov <s.a.skuratov@gmail.com>
2020-11-24 13:41:30 +0600

    Added 509 Solution #2 with Memoization

--- solutions/src/main/java/io/bigtotoro/_509.java
+++ solutions/src/main/java/io/bigtotoro/_509.java
@@ -1,5 +1,8 @@
 package io.bigtotoro;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <a href="https://leetcode.com/problems/fibonacci-number/">509. Fibonacci Number</a>
  */
@@ -8,13 +11,47 @@ public class _509 {
     public static class Solution1 {
         /**
          * Time: O(n)
-         * Space: O(n)
+         * Space: O(1)
          */
         public int fib(int N) {
             if (N < 2) {
                 return N;
             }
             return fib(N - 1) + fib(N - 2);
+        }
+    }
+
+    public static class Solution2 {
+
+        private Map<Integer, Integer> cache = new HashMap<>();
+
+        {
+            cache.put(0, 0);
+            cache.put(1, 1);
+        }
+
+        public int fib(int N) {
+            if (N < 2) {
+                return N;
+            }
+
+            Integer l = 0;
+            Integer r = 0;
+            if (cache.containsKey(N - 1)) {
+                l = cache.get(N - 1);
+            } else {
+                l = fib(N - 1);
+                cache.put(N - 1, l);
+            }
+
+            if (cache.containsKey(N - 2)) {
+                r = cache.get(N - 2);
+            } else {
+                r = fib(N - 2);
+                cache.put(N - 2, r);
+            }
+
+            return l + r;
         }
     }
 }
--- solutions/src/test/java/io/bigtotoro/_509_Test.java
+++ solutions/src/test/java/io/bigtotoro/_509_Test.java
@@ -6,39 +6,71 @@ import org.junit.Test;
 public class _509_Test {
 
     private static _509.Solution1 solution1;
+    private static _509.Solution2 solution2;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _509.Solution1();
+        solution2 = new _509.Solution2();
     }
 
     @Test
-    public void test1() {
+    public void test11() {
         assert(solution1.fib(0) == 0);
     }
 
     @Test
-    public void test2() {
+    public void test12() {
         assert(solution1.fib(1) == 1);
     }
 
     @Test
-    public void test3() {
+    public void test13() {
         assert(solution1.fib(2) == 1);
     }
 
     @Test
-    public void test4() {
+    public void test14() {
         assert(solution1.fib(3) == 2);
     }
 
     @Test
-    public void test5() {
+    public void test15() {
         assert(solution1.fib(4) == 3);
     }
 
     @Test
-    public void test6() {
+    public void test16() {
         assert(solution1.fib(24) == 46368);
+    }
+
+    @Test
+    public void test21() {
+        assert(solution2.fib(0) == 0);
+    }
+
+    @Test
+    public void test22() {
+        assert(solution2.fib(1) == 1);
+    }
+
+    @Test
+    public void test23() {
+        assert(solution2.fib(2) == 1);
+    }
+
+    @Test
+    public void test24() {
+        assert(solution2.fib(3) == 2);
+    }
+
+    @Test
+    public void test25() {
+        assert(solution2.fib(4) == 3);
+    }
+
+    @Test
+    public void test26() {
+        assert(solution2.fib(24) == 46368);
     }
 }

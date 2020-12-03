31da934d1a00cb7461fd172786fd3e489157217f
sskuratov <s.a.skuratov@gmail.com>
2020-12-03 12:24:24 +0600

    Added 183

---
+++ solutions/database/_183.sql
@@ -0,0 +1,21 @@
+/**
+ * <a href="https://leetcode.com/problems/customers-who-never-order/">183. Customers Who Never Order</a>
+ */
+
+/**
+  Solution 1
+ */
+SELECT c.name AS Customers
+FROM Customers c
+WHERE c.id NOT IN (
+    SELECT DISTINCT customerId
+    FROM Orders o);
+
+/**
+  Solution 1
+ */
+SELECT c.name AS Customers
+FROM Customers c
+         LEFT JOIN Orders o
+                   ON c.id = o.customerId
+WHERE o.customerId IS NULL;

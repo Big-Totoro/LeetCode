abde4dfab0c81bb2d2131affde21c5f861053cf1
sskuratov <s.a.skuratov@gmail.com>
2020-12-10 15:29:38 +0600

    Added 210 Solution #1

--- solutions/src/main/java/io/bigtotoro/_210.java
+++ solutions/src/main/java/io/bigtotoro/_210.java
@@ -1,5 +1,7 @@
 package io.bigtotoro;
 
+import java.util.*;
+
 /**
  * <a href="https://leetcode.com/problems/course-schedule-ii/">210. Course Schedule II</a>
  */
@@ -7,9 +9,99 @@ package io.bigtotoro;
 public class _210 {
 
     public static class Solution1 {
-        public int[] findOrder(int numCourses, int[][] prerequisites) {
 
-            return null;
+        private boolean cycle = false;
+
+        public enum Color {
+            WHITE, GRAY, BLACK
+        }
+
+        private Map<Integer, List<Integer>> adjacencyMap = new HashMap<>();
+        private Map<Integer, Color> colorMap = new HashMap<>();
+        private LinkedList<Integer> result = new LinkedList<>();
+
+        public int[] findOrder(int numCourses, int[][] prerequisites) {
+            if (numCourses == 0) {
+                return new int[0];
+            }
+
+            /**
+             * Initialize vertices colors
+             */
+            for (int i = 0; i < numCourses; ++i) {
+                colorMap.put(i, Color.WHITE);
+            }
+
+            /**
+             * Create Adjacency list bases on prerequisites list
+             */
+            for (int i = 0; i < prerequisites.length; ++i) {
+                int dst = prerequisites[i][0];
+                int src = prerequisites[i][1];
+
+                List<Integer> dstList = adjacencyMap.getOrDefault(src, new ArrayList<>());
+                dstList.add(dst);
+                adjacencyMap.put(src, dstList);
+            }
+
+            for (int i = 0; i < numCourses; ++i) {
+                if (colorMap.get(i) == Color.WHITE) {
+                    dfs(i);
+                }
+            }
+
+            /**
+             * Return the empty array if we detected the cycle.
+             */
+            if (cycle == true) {
+                return new int[0];
+            }
+
+            /**
+             * Copy the result topologically sorted list to the array
+             */
+            int[] temp = new int[result.size()];
+            for (int i = 0; i < result.size(); ++i) {
+               temp[i] = result.get(i);
+            }
+
+            return temp;
+        }
+
+        private void dfs(int node) {
+            /**
+             * Check for the cycle and stop if the cycle was detected.
+             */
+            if (cycle == true) {
+                return;
+            }
+
+            colorMap.put(node, Color.GRAY);
+
+            List<Integer> adjacencyNodes = adjacencyMap.getOrDefault(node, new ArrayList<>());
+            for (Integer adjacencyNode : adjacencyNodes) {
+                /**
+                 * If the node has not been visited then go with it
+                 */
+                if (colorMap.get(adjacencyNode) == Color.WHITE) {
+                    dfs(adjacencyNode);
+                } else if (colorMap.get(adjacencyNode) == Color.GRAY) {
+                    /**
+                     * If we marked the node as GRAY (in process) and visit the node again then we have the cycle.
+                     * It means we will not be abel to topologically sort the graph.
+                     */
+                    cycle = true;
+                }
+            }
+            /**
+             * Mark the node as visited
+             */
+            colorMap.put(node, Color.BLACK);
+
+            /**
+             * Add node to the list in the proper topologically sorted order
+             */
+            result.addFirst(node);
         }
     }
 }
--- solutions/src/test/java/io/bigtotoro/_210_Test.java
+++ solutions/src/test/java/io/bigtotoro/_210_Test.java
@@ -6,22 +6,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class _210_Test {
 
-    private static _210.Solution1 solution1 = new _210.Solution1();
-
     @Test
     public void test11() {
+        _210.Solution1 solution1 = new _210.Solution1();
         assertThat(solution1.findOrder(2, new int[][] {{1, 0}}))
             .isEqualTo(new int[] {0, 1});
     }
 
     @Test
     public void test12() {
+        _210.Solution1 solution1 = new _210.Solution1();
         assertThat(solution1.findOrder(4, new int[][] {{1, 0}, {2, 0}, {3, 1}, {3, 2}}))
             .isEqualTo(new int[] {0, 2, 1, 3});
     }
 
     @Test
     public void test13() {
+        _210.Solution1 solution1 = new _210.Solution1();
         assertThat(solution1.findOrder(1, new int[][] {}))
             .isEqualTo(new int[] {0});
     }

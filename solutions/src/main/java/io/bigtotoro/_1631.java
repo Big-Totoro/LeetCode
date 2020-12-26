f82379717d299063be6355ae6d48f043af43af95
sskuratov <s.a.skuratov@gmail.com>
2020-12-26 10:35:24 +0600

    Added 1631 Solution #1

--- solutions/src/main/java/io/bigtotoro/_1631.java
+++ solutions/src/main/java/io/bigtotoro/_1631.java
@@ -7,6 +7,69 @@ package io.bigtotoro;
 public class _1631 {
 
     public static class Solution1 {
+
+        private int pathWithMinEfforts = Integer.MAX_VALUE;
+
+        public int minimumEffortPath(int[][] heights) {
+            if (heights.length == 0 || heights[0].length == 0) {
+                return 0;
+            }
+
+            int result = dfs(heights, 0, 0, heights.length - 1, heights[0].length - 1, 0);
+
+            return result;
+        }
+
+        private int dfs(int[][] heights, int row, int col, int endRow, int endCol, int maxDifference) {
+            if (row == endRow && col == endCol) {
+                pathWithMinEfforts = Math.min(pathWithMinEfforts, maxDifference);
+
+                return maxDifference;
+            }
+
+            int currentHeight = heights[row][col];
+            heights[row][col] = 0;
+
+            int minEffort = Integer.MAX_VALUE;
+            if (col - 1 >= 0 && heights[row][col - 1] != 0) {
+                int currentDifference = Math.abs(currentHeight - heights[row][col - 1]);
+                int maxCurrentDifference = Math.max(maxDifference, currentDifference);
+                if (maxCurrentDifference < pathWithMinEfforts) {
+                    int result = dfs(heights, row, col - 1, endRow, endCol, maxCurrentDifference);
+                    minEffort = Math.min(minEffort, result);
+                }
+            }
+            if (row - 1 >= 0 && heights[row - 1][col] != 0) {
+                int currentDifference = Math.abs(currentHeight - heights[row - 1][col]);
+                int maxCurrentDifference = Math.max(maxDifference, currentDifference);
+                if (maxCurrentDifference < pathWithMinEfforts) {
+                    int result = dfs(heights, row - 1, col, endRow, endCol, maxCurrentDifference);
+                    minEffort = Math.min(minEffort, result);
+                }
+            }
+            if (col + 1 <= endCol && heights[row][col + 1] != 0) {
+                int currentDifference = Math.abs(currentHeight - heights[row][col + 1]);
+                int maxCurrentDifference = Math.max(maxDifference, currentDifference);
+                if (maxCurrentDifference < pathWithMinEfforts) {
+                    int result = dfs(heights, row, col + 1, endRow, endCol, maxCurrentDifference);
+                    minEffort = Math.min(minEffort, result);
+                }
+            }
+            if (row + 1 <= endRow && heights[row + 1][col] != 0) {
+                int currentDifference = Math.abs(currentHeight - heights[row + 1][col]);
+                int maxCurrentDifference = Math.max(maxDifference, currentDifference);
+                if (maxCurrentDifference < pathWithMinEfforts) {
+                    int result = dfs(heights, row + 1, col, endRow, endCol, maxCurrentDifference);
+                    minEffort = Math.min(minEffort, result);
+                }
+            }
+            heights[row][col] = currentHeight;
+
+            return minEffort;
+        }
+    }
+
+    public static class Solution2 {
         public int minimumEffortPath(int[][] heights) {
             if (heights.length == 0 || heights[0].length == 0) {
                 return 0;
--- solutions/src/test/java/io/bigtotoro/_1631_Test.java
+++ solutions/src/test/java/io/bigtotoro/_1631_Test.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class _1631_Test {
 
     private static _1631.Solution1 solution1 = new _1631.Solution1();
+    private static _1631.Solution2 solution2 = new _1631.Solution2();
 
     @Test
     public void test11() {
@@ -21,6 +22,22 @@ public class _1631_Test {
     @Test
     public void test13() {
         assertThat(solution1.minimumEffortPath(new int[][]{{1, 2, 1, 1, 1}, {1, 2, 1, 2, 1}, {1, 2, 1, 2, 1},
+                {1, 2, 1, 2, 1}, {1, 1, 1, 2, 1}})).isEqualTo(0);
+    }
+
+    @Test
+    public void test21() {
+        assertThat(solution2.minimumEffortPath(new int[][]{{1, 2, 2}, {3, 8, 2}, {5, 3, 5}})).isEqualTo(2);
+    }
+
+    @Test
+    public void test22() {
+        assertThat(solution2.minimumEffortPath(new int[][]{{1, 2, 3}, {3, 8, 4}, {5, 3, 5}})).isEqualTo(1);
+    }
+
+    @Test
+    public void test23() {
+        assertThat(solution2.minimumEffortPath(new int[][]{{1, 2, 1, 1, 1}, {1, 2, 1, 2, 1}, {1, 2, 1, 2, 1},
                 {1, 2, 1, 2, 1}, {1, 1, 1, 2, 1}})).isEqualTo(0);
     }
 }

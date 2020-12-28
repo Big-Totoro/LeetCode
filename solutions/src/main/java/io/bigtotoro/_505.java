298588a12e2819058751d461046e2a6144d78b6f
sskuratov <s.a.skuratov@gmail.com>
2020-12-28 13:14:52 +0600

    Added 505 Solution #1

--- solutions/src/main/java/io/bigtotoro/_505.java
+++ solutions/src/main/java/io/bigtotoro/_505.java
@@ -1,5 +1,7 @@
 package io.bigtotoro;
 
+import java.util.Arrays;
+
 /**
  * <a href="https://leetcode.com/problems/the-maze-ii/">505. The Maze II</a>
  */
@@ -8,7 +10,52 @@ public class _505 {
 
     public static class Solution1 {
 
+        public int shortestDistance(int[][] maze, int[] start, int[] destination) {
+            int[][] distanceMaze = new int[maze.length][maze[0].length];
+            for (int i = 0; i < distanceMaze.length; ++i) {
+                Arrays.fill(distanceMaze[i], Integer.MAX_VALUE);
+            }
+            distanceMaze[start[0]][start[1]] = 0;
+
+            dfs(maze, start, distanceMaze);
+
+            return (distanceMaze[destination[0]][destination[1]] == Integer.MAX_VALUE) ? -1 : distanceMaze[destination[0]][destination[1]];
+        }
+
+        private void dfs(int[][] maze, int[] start, int[][] distanceMaze) {
+            int startRow = start[0];
+            int startCol = start[1];
+            int[][] directions = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+            for (int[] direction : directions) {
+                int row = startRow + direction[0];
+                int col = startCol + direction[1];
+
+                int steps = 0;
+                while (isValid(maze, row, col) && maze[row][col] == 0) {
+                    row += direction[0];
+                    col += direction[1];
+                    ++steps;
+                }
+
+                int cellRow = row - direction[0];
+                int cellCol = col - direction[1];
+                if (isValid(maze, cellRow, cellCol) && distanceMaze[cellRow][cellCol] > distanceMaze[startRow][startCol] + steps) {
+                    distanceMaze[cellRow][cellCol] = distanceMaze[startRow][startCol] + steps;
+                    dfs(maze, new int[] {cellRow, cellCol}, distanceMaze);
+                }
+            }
+        }
+
+        private boolean isValid(int[][] maze, int r, int c) {
+            return r >= 0 && c >= 0 && r < maze.length && c < maze[0].length;
+        }
+    }
+
+    public static class Solution2 {
+
         private int minDistance = Integer.MAX_VALUE;
+
         public int shortestDistance(int[][] maze, int[] start, int[] destination) {
             dfs(maze, start, destination);
 
@@ -16,6 +63,20 @@ public class _505 {
         }
 
         private void dfs(int[][] maze, int[] start, int[] destination) {
+            int[][] directions = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+            for (int[] direction : directions) {
+                int x = start[0] + direction[0];
+                int y = start[1] + direction[1];
+
+                if (isValid(maze, x, y) && maze[x][y] > 0) {
+
+                }
+            }
         }
+        private boolean isValid(int[][] maze, int x, int y) {
+            return x >= 0 && y >= 0 && x < maze.length && y < maze[0].length;
+        }
+
     }
 }
--- solutions/src/test/java/io/bigtotoro/_505_Test.java
+++ solutions/src/test/java/io/bigtotoro/_505_Test.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class _505_Test {
 
     private static final _505.Solution1 SOLUTION_1 = new _505.Solution1();
+    private static final _505.Solution2 SOLUTION_2 = new _505.Solution2();
 
     @Test
     public void test11() {
@@ -22,6 +23,28 @@ public class _505_Test {
     @Test
     public void test12() {
         assertThat(SOLUTION_1.shortestDistance(new int[][] {
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0},
+                {1, 1, 0, 1, 1},
+                {0, 0, 0, 0, 0}
+        }, new int[] {0, 4}, new int[] {3, 2})).isEqualTo(-1);
+    }
+
+    @Test
+    public void test21() {
+        assertThat(SOLUTION_2.shortestDistance(new int[][] {
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0},
+                {1, 1, 0, 1, 1},
+                {0, 0, 0, 0, 0}
+        }, new int[] {0, 4}, new int[] {4, 4})).isEqualTo(12);
+    }
+
+    @Test
+    public void test22() {
+        assertThat(SOLUTION_2.shortestDistance(new int[][] {
                 {0, 0, 1, 0, 0},
                 {0, 0, 0, 0, 0},
                 {0, 0, 0, 1, 0},

d2a6e6bf356ee51fb27daaebca0cf0ae64098a78
sskuratov <s.a.skuratov@gmail.com>
2021-01-01 12:34:20 +0600

    Added 257 Solution #2

--- solutions/src/main/java/io/bigtotoro/_257.java
+++ solutions/src/main/java/io/bigtotoro/_257.java
@@ -1,7 +1,9 @@
 package io.bigtotoro;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * <a href="https://leetcode.com/problems/cheapest-flights-within-k-stops/">787. Cheapest Flights Within K Stops</a>
@@ -44,6 +46,39 @@ public class _257 {
                     dfs(root.right, path, result);
                 }
             }
+        }
+    }
+
+    public static class Solution2 {
+        public List<String> binaryTreePaths(TreeNode root) {
+            if (root == null) {
+                return List.of();
+            }
+
+            List<String> result = new ArrayList<>();
+            Queue<TreeNode> nodeQueue = new LinkedList<>();
+            Queue<String> pathQueue = new LinkedList<>();
+            nodeQueue.offer(root);
+            pathQueue.offer(Integer.toString(root.val));
+
+            while (!nodeQueue.isEmpty()) {
+                TreeNode topNode = nodeQueue.poll();
+                String pathNode = pathQueue.poll();
+
+                if (topNode.left == null && topNode.right == null) {
+                    result.add(pathNode);
+                }
+                if (topNode.left != null) {
+                    nodeQueue.offer(topNode.left);
+                    pathQueue.offer(pathNode + "->" + topNode.left.val);
+                }
+                if (topNode.right != null) {
+                    nodeQueue.offer(topNode.right);
+                    pathQueue.offer(pathNode + "->" + topNode.right.val);
+                }
+            }
+
+            return result;
         }
     }
 }
--- solutions/src/test/java/io/bigtotoro/_257_Test.java
+++ solutions/src/test/java/io/bigtotoro/_257_Test.java
@@ -9,10 +9,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class _257_Test {
 
     private static final _257.Solution1 SOLUTION_1 = new _257.Solution1();
+    private static final _257.Solution2 SOLUTION_2 = new _257.Solution2();
 
     @Test
     public void test11() {
         assertThat(SOLUTION_1.binaryTreePaths(
+                new _257.TreeNode(1,
+                        new _257.TreeNode(2,
+                                null,
+                                new _257.TreeNode(5)
+                        ),
+                        new _257.TreeNode(3))
+            )
+        ).isEqualTo(List.of("1->2->5", "1->3"));
+    }
+
+    @Test
+    public void test21() {
+        assertThat(SOLUTION_2.binaryTreePaths(
                 new _257.TreeNode(1,
                         new _257.TreeNode(2,
                                 null,
